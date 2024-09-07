da80821c964892463bd2959d91a924b3d8316ee6
shsat94 <shsat94@gmail.com>
2024-09-08 03:52:29 +0530

    rooms are working

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 const io = require('socket.io')(8000, { cors: { origin: "*" } });
 let rooms = [];
 
+
+
+
+const generateRoomId = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < 7; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+    }
+
+    return result;
+}
+
 io.on('connection', socket => {
-    socket.on('create-room', (roomId, hostName) => {
+    const roomId= generateRoomId();
+    socket.on('create-room', (hostName) => {
         if (rooms.length < 1) {
+
             socket.join(roomId);
             let roomdetails = {
                 roomId: roomId,
@@ -11,7 +28,8 @@ io.on('connection', socket => {
                 members: []
             }
             rooms.push(roomdetails);
-            console.log(roomdetails);
+            console.log(rooms);
+            
             return;
         }
         for (i = 0; i <= rooms.length; i++) {
@@ -27,7 +45,8 @@ io.on('connection', socket => {
                     members: []
                 }
                 rooms.push(roomdetails);
-                console.log(roomdetails);
+                console.log(rooms);
+                
                 break;
             }
 
@@ -42,10 +61,10 @@ io.on('connection', socket => {
                 rooms[i].members.push(name);
                 console.log(rooms);
                 console.log(`${name} joined RoomID ${roomId}`);
+                io.to(roomId).emit('room-details',name);
                 break;
             }
         }
-
     });
 
 });
